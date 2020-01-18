c61d60e3ee80ff1c480bf835fb6e9c67e9ee66b9
deepti <deepti@gmail.com>
2020-01-18 00:40:38 -0800

    commit

---
+++ portopen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import socket
+import sys
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(2)                                      #2 Second Timeout
+result = sock.connect_ex(('127.0.0.1',80))
+if result == 0:
+	print('port OPEN')
+else:
+	print('port CLOSED')
